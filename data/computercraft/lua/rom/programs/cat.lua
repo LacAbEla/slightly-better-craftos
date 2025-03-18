8b0cc659c2e0e0495ee1c54cf53c9b363aa8dc9a
LacAbEla <47212432+LacAbEla@users.noreply.github.com>
2025-03-18 21:27:20 +0100

    cat.lua: use pagedPrint to print the file contents, improve filename header

--- data/computercraft/lua/rom/programs/cat.lua
+++ data/computercraft/lua/rom/programs/cat.lua
@@ -4,10 +4,29 @@ if not filename then
     return
 end
 
+
+-- Read file contents
+local contents
 local file, errorMsg = fs.open(shell.resolve(filename), "r")
 if file == nil then
     error("Error while opening file: " .. errorMsg, 0)
 end
-print("=== " .. filename .. " ===")
-print(file.readAll())
+contents = file.readAll()
 file.close()
+
+-- Print header with the filename in one line
+local originalColor = term.getTextColor()
+term.setTextColor(term.isColor() and colors.lightBlue or colors.lightGray)
+local termWidth, _ = term.getSize()
+if termWidth < 12 then
+    print(filename:sub(1, termWidth))
+elseif #filename > termWidth - 8 then -- 8 is the length of the header borders
+    print("=== " .. filename:sub(1, termWidth - 11) .. "... ===") -- 11 is the 8 from above plus the three dots
+else
+    print("=== " .. filename .. " ===")
+end
+term.setTextColor(originalColor)
+
+-- Paged print of the file contents
+local _, posHeight = term.getCursorPos()
+textutils.pagedPrint(contents, posHeight - 3)
